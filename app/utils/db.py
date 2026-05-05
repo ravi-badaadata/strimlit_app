c65a165165c7f61a05c72362d2dfa3b974830bc8
ravi-badaadata <ravisingh324@gmail.com>
2026-05-05 13:12:47 +0530

    Add database session utility function

---
+++ app/utils/db.py
@@ -0,0 +1,14 @@
+from snowflake.snowpark.session import Session
+import streamlit as st
+
+def get_session():
+    connection_parameters = {
+        "account": st.secrets["account"],
+        "user": st.secrets["user"],
+        "password": st.secrets["password"],
+        "role": st.secrets["role"],
+        "warehouse": st.secrets["warehouse"],
+        "database": st.secrets["database"],
+        "schema": st.secrets["schema"],
+    }
+    return Session.builder.configs(connection_parameters).create()
