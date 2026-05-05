e94651831642cdc6bfd140f41389ea7b42b7ca4b
ravi-badaadata <ravisingh324@gmail.com>
2026-05-05 17:56:41 +0530

    Refactor get_session to use get_active_session

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,14 +1,4 @@
-from snowflake.snowpark.session import Session
-import streamlit as st
+from snowflake.snowpark.context import get_active_session
 
 def get_session():
-    connection_parameters = {
-        "account": st.secrets["account"],
-        "user": st.secrets["user"],
-        "password": st.secrets["password"],
-        "role": st.secrets["role"],
-        "warehouse": st.secrets["warehouse"],
-        "database": st.secrets["database"],
-        "schema": st.secrets["schema"],
-    }
-    return Session.builder.configs(connection_parameters).create()
+    return get_active_session()
