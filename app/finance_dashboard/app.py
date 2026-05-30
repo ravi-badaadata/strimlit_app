2a2617f0f39b67f1ac5942b2edb10dfa07d33b48
ravi-badaadata <ravisingh324@gmail.com>
2026-05-31 01:30:37 +0530

    new1

---
+++ app/finance_dashboard/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Sales Dashboard")
+
+st.write("Sale FINANCE Application")
---
+++ app/sales_dashboard/output/bundle/streamlit/SALES_DASHBOARD_APP/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Sales Dashboard")
+
+st.write("Sales Dashboard Application")
