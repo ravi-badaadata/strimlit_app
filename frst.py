0bd8ed0a0246bff606ad46c67d73d69fbc6adf82
ravi-badaadata <ravisingh324@gmail.com>
2026-05-30 18:46:47 +0530

    abc

---
+++ frst.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from utils.db import get_session
+
+st.title("My Snowflake Streamlit app badaadata_new 🚀")
+
+session = get_session()
+
+df = session.sql("SELECT CURRENT_USER(), CURRENT_DATABASE()").to_pandas()
+
+st.dataframe(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
